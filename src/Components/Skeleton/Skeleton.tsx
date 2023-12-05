4ba1b18d6c3bd8c19964e5272f8fd409a7594aca
paksasha76 <pak.aleksandr2001@mail.ru>
2023-12-05 21:48:13 +0300

    project

--- src/Components/Skeleton/Skeleton.tsx
+++ src/Components/Skeleton/Skeleton.tsx
@@ -3,19 +3,19 @@ import ContentLoader from "react-content-loader";
 
 const MyLoader: FC<{}> = (props) => (
   <ContentLoader
-    speed={2}
-    width={1000}
-    height={100}
-    viewBox="0 0 1200 120"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
-    {...props}
-  >
-    <circle cx="14" cy="1000" r="5" />
-    <rect x="269" y="21" rx="0" ry="50" width="1" height="100" />
-    <rect x="6" y="8" rx="0" ry="50" width="1200" height="500" />
-    <rect x="401" y="300" rx="0" ry="50" width="24" height="300" />
-    <rect x="403" y="400" rx="17" ry="50" width="21" height="370" />
+  speed={2}
+  width={1400}
+  height={150}
+  viewBox="0 0 1400 150"
+  backgroundColor="grey"
+  foregroundColor="#ecebeb"
+  {...props}
+>
+  <rect x="19" y="912" rx="11" ry="11" width="552" height="101" /> 
+  <rect x="383" y="703" rx="0" ry="0" width="46" height="1" /> 
+  <rect x="386" y="710" rx="0" ry="0" width="1" height="0" /> 
+  <rect x="16" y="43" rx="0" ry="0" width="107" height="0" /> 
+  <rect x="17" y="25" rx="10" ry="10" width="1000" height="115" />
   </ContentLoader>
 );
 
